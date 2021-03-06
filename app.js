785f7a2c86f5fa1f7fac3c573d444dad00a8de02
Emilija Malogajski <emalogajski@gmail.com>
2021-03-06 17:42:57 +0100

    Making header sticky and working on language change button

---
+++ app.js
@@ -0,0 +1,19 @@
+let navigationMenu;
+
+function stickyHeader () {
+  navigationMenu = document.getElementById('nav-list');
+  sticky = navigationMenu.offsetTop;
+
+  if(window.pageYOffset > sticky) {
+    navigationMenu.classList.add('sticky');
+  } else {
+    navigationMenu.classList.remove('sticky');
+  }
+}
+
+function init () {
+  //Function for making header sticky
+  window.addEventListener('scroll', stickyHeader);
+}
+
+window.onload = init;
