3b54ec7875a96a9ddf1f2e03ad861a362f3bcceb
Emilija Malogajski <emalogajski@gmail.com>
2021-03-08 21:49:12 +0100

    Adding dresscode picture and styling

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let navigationMenu;
+const dresscodeInstructions = document.getElementById('how-to-dress');
+const dresscodeImage = document.getElementById('comfy-shoes-img');
 
 function stickyHeader () {
   navigationMenu = document.getElementById('nav-list');
@@ -11,10 +13,17 @@ function stickyHeader () {
   }
 }
 
+function showHideDresscode () {
+  dresscodeInstructions.classList.toggle('how-to-dress');
+  dresscodeImage.classList.toggle('how-to-dress-img');
+}
+
 function init () {
   //Function for making header sticky
   window.addEventListener('scroll', stickyHeader);
-  
 }
 
 window.onload = init;
+
+//Function for showing dress code on hover
+dresscodeImage.addEventListener('mouseover', showHideDresscode);
