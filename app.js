5c2f3cf20d2c6956050f63c4127430ed9a401024
Emilija Malogajski <emalogajski@gmail.com>
2021-03-09 07:21:31 +0100

    Styling dress code section and changing file structure to display German version first

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let navigationMenu;
 const dresscodeInstructions = document.getElementById('how-to-dress');
+const dresscodeDiv = document.getElementById('dress-code');
+const dresscodeTitle = document.getElementById('dress-code-title');
 const dresscodeImage = document.getElementById('comfy-shoes-img');
 
 function stickyHeader () {
@@ -13,17 +15,35 @@ function stickyHeader () {
   }
 }
 
-function showHideDresscode () {
-  dresscodeInstructions.classList.toggle('how-to-dress');
-  dresscodeImage.classList.toggle('how-to-dress-img');
+// function showHideDresscode () {
+//   dresscodeInstructions.classList.toggle('how-to-dress');
+//   dresscodeImage.classList.toggle('how-to-dress-img');
+//   dresscodeTitle.classList.toggle('alternate');
+// }
+
+function showDresscode () {
+  dresscodeInstructions.classList.add('how-to-dress');
+  dresscodeImage.classList.add('how-to-dress-img');
+  dresscodeTitle.classList.add('alternate');
+  dresscodeTitle.classList.remove('dress-code-title');
+}
+
+function hideDresscode () {
+  dresscodeInstructions.classList.remove('how-to-dress');
+  dresscodeImage.classList.remove('how-to-dress-img');
+  dresscodeTitle.classList.remove('alternate');
+  dresscodeTitle.classList.add('dress-code-title');
 }
 
 function init () {
   //Function for making header sticky
   window.addEventListener('scroll', stickyHeader);
+  
+  //Function for showing dress code on hover
+  dresscodeImage.addEventListener('mouseenter', showDresscode);
+  dresscodeImage.addEventListener('mouseout', hideDresscode);
 }
 
 window.onload = init;
 
-//Function for showing dress code on hover
-dresscodeImage.addEventListener('mouseover', showHideDresscode);
+
