7d413b056c4984e97d674f84214074a4fc75f545
Emilija Malogajski <emalogajski@gmail.com>
2021-03-11 18:26:03 +0100

    Adding present section

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 let navigationMenu;
-const dresscodeInstructions = document.getElementById('how-to-dress');
-const dresscodeDiv = document.getElementById('dress-code');
-const dresscodeTitle = document.getElementById('dress-code-title');
-const dresscodeImage = document.getElementById('comfy-shoes-img');
+let sticky;
+let home;
 
 function stickyHeader () {
   navigationMenu = document.getElementById('nav-list');
@@ -15,35 +13,9 @@ function stickyHeader () {
   }
 }
 
-// function showHideDresscode () {
-//   dresscodeInstructions.classList.toggle('how-to-dress');
-//   dresscodeImage.classList.toggle('how-to-dress-img');
-//   dresscodeTitle.classList.toggle('alternate');
-// }
-
-function showDresscode () {
-  dresscodeInstructions.classList.add('how-to-dress');
-  dresscodeImage.classList.add('how-to-dress-img');
-  dresscodeTitle.classList.add('alternate');
-  dresscodeTitle.classList.remove('dress-code-title');
-}
-
-function hideDresscode () {
-  dresscodeInstructions.classList.remove('how-to-dress');
-  dresscodeImage.classList.remove('how-to-dress-img');
-  dresscodeTitle.classList.remove('alternate');
-  dresscodeTitle.classList.add('dress-code-title');
-}
-
 function init () {
   //Function for making header sticky
   window.addEventListener('scroll', stickyHeader);
-  
-  //Function for showing dress code on hover
-  dresscodeImage.addEventListener('mouseenter', showDresscode);
-  dresscodeImage.addEventListener('mouseout', hideDresscode);
 }
 
 window.onload = init;
-
-
